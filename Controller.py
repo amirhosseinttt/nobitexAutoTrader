d3b580cd6e9c9ad6c2543b710c3c255677068377
amirhossein <torkanlooa@gmail.com>
2021-11-01 20:49:09 +0330

    logger added and login completed

--- Controller.py
+++ Controller.py
@@ -1,6 +1,10 @@
 import pyotp
 from Model import login
 from keys import keys
+from time import sleep
+from logger import err_log
+
+TOKEN = ""
 
 
 def get_2fa(based32: str):
@@ -8,14 +12,37 @@ def get_2fa(based32: str):
     return totp.now()
 
 
-def request_token():
+def request_new_token():
+    exception = ""
+    response = None
     username = keys.key_dict['username']
     password = keys.key_dict['password']
     otp = str(get_2fa(keys.key_dict['2fa-backup']))
-    response = login(username, password, otp)
-    print(response.text)
+    try:
+        response = login(username, password, otp)
+    except Exception as e:
+        response = None
+        exception = str(e)
+
+    while response is None or response.status_code != 200:
+        if response is None:
+            err_log("login requests failed to send!", exception, 1)
+        else:
+            err_log("login error", response.text, 1)
+        sleep(5)
+        username = keys.key_dict['username']
+        password = keys.key_dict['password']
+        otp = str(get_2fa(keys.key_dict['2fa-backup']))
+        try:
+            response = login(username, password, otp)
+        except Exception as e:
+            response = None
+            exception = str(e)
+
+        if response != None and response.status_code == 200:
+            err_log("login fixed", response.text, 200)
+
+    TOKEN = response.json()['key']
 
 
 
-if __name__ == "__main__":
-    print(request_token())
---
+++ logger.py
@@ -0,0 +1,4 @@
+def err_log(name: str, text: str, code=0):
+    print(name, text, code)
+
+
