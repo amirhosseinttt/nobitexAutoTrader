0a2031a4365aabba028175e1143be1783b0aed97
amirhossein <torkanlooa@gmail.com>
2021-11-01 21:01:18 +0330

    controller changed to class base struct

--- Controller.py
+++ Controller.py
@@ -4,45 +4,41 @@ from keys import keys
 from time import sleep
 from logger import err_log
 
-TOKEN = ""
 
+class Controller:
+    TOKEN = ""
 
-def get_2fa(based32: str):
-    totp = pyotp.TOTP(based32)
-    return totp.now()
+    def _get_2fa(self, based32: str):
+        totp = pyotp.TOTP(based32)
+        return totp.now()
 
-
-def request_new_token():
-    exception = ""
-    response = None
-    username = keys.key_dict['username']
-    password = keys.key_dict['password']
-    otp = str(get_2fa(keys.key_dict['2fa-backup']))
-    try:
-        response = login(username, password, otp)
-    except Exception as e:
-        response = None
-        exception = str(e)
-
-    while response is None or response.status_code != 200:
-        if response is None:
-            err_log("login requests failed to send!", exception, 1)
-        else:
-            err_log("login error", response.text, 1)
-        sleep(5)
+    def request_new_token(self):
+        exception = ""
         username = keys.key_dict['username']
         password = keys.key_dict['password']
-        otp = str(get_2fa(keys.key_dict['2fa-backup']))
+        otp = str(self._get_2fa(keys.key_dict['2fa-backup']))
         try:
             response = login(username, password, otp)
         except Exception as e:
             response = None
             exception = str(e)
 
-        if response != None and response.status_code == 200:
-            err_log("login fixed", response.text, 200)
+        while response is None or response.status_code != 200:
+            if response is None:
+                err_log("login requests failed to send!", exception, 1)
+            else:
+                err_log("login error", response.text, 1)
+            sleep(5)
+            username = keys.key_dict['username']
+            password = keys.key_dict['password']
+            otp = str(self._get_2fa(keys.key_dict['2fa-backup']))
+            try:
+                response = login(username, password, otp)
+            except Exception as e:
+                response = None
+                exception = str(e)
 
-    TOKEN = response.json()['key']
+            if response is not None and response.status_code == 200:
+                err_log("login fixed", response.text, 200)
 
-
-
+        self.TOKEN = response.json()['key']
---
+++ main.py
@@ -0,0 +1,4 @@
+from Controller import Controller
+
+if __name__ == "__main__":
+    print("System started...")
