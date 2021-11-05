b0322818ef96736f0d73ad3052233630604944ef
amirhossein <torkanlooa@gmail.com>
2021-11-05 18:50:42 +0330

    log output has been reshaped

--- logger.py
+++ logger.py
@@ -5,6 +5,7 @@ def err_log(name: str, text: str, code=0):
     current_time = datetime.datetime.now()
     print(name, text, code, datetime)
     with open("log.txt", "at") as log_file:
-        string = name + "\n" + text + "\n" + str(code) + "\n" + str(current_time)
+        string = "Subject: " + name + "\n" + "Test: " + text + "\n" + "Code: " + str(code) + "\n" + "Time:" + \
+                 str(current_time)
         string += "\n" * 3 + '-' * 30 + "\n" * 3
         log_file.write(string)
